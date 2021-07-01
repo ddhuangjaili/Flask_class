f39c0cc77f0931b118f2577ce1d7c69646a01f2a
perry <huangjiali_007@126.com>
2021-07-01 17:38:31 +0800

    Flask study学习工程 ——README记录 by 7.1

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# Flask 框架学习工程
+# _Flask 框架学习工程_
+## 目录
+#### 项目目录
+#### 课程目录
+
+#### _环境安装说明_
+_本地(windows)<br>
+python版本：3.8.3<br>
+Flask版本：2.0.1<br>
+click版本：8.0.1<br>
+colorama版本：0.4.4<br>
+itsdangerous版本：2.0.1<br>
+Jinja2版本：3.0.1<br>
+MarkupSafe版本：2.0.1<br>
+Werkzeug版本：2.0.1<br>_
--- app.py
+++ app.py
@@ -1,12 +1,22 @@
-from flask import Flask
+from flask import Flask, redirect
 
 app = Flask(__name__)
 
-
+# 首页请求
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
+# 带参数的get请求
+@app.route('/user/<name>')
+def user(name):
+    return 'Hello {}!'.format(name)
 
+# 重定向
+@app.route('/redirect')
+def nologin():
+    return redirect("http://www.baidu.com")
+
+# 主函数
 if __name__ == '__main__':
     app.run()
